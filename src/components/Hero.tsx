3d11cb8be8d7f5ed00a402a5f988dad5ea693949
Paul Roubinet <paul.roubinet@etu.univ-grenoble-alpes.fr>
2026-02-26 16:22:12 +0100

    Fix Hero component height class for consistent styling across screen sizes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -88,7 +88,7 @@ function Hero() {
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5, delay: 0.4, ease }}
-        className="animate-spin-slow w-screen h-100vw md:w-[80vw] md:h-[80vw] bottom-0 sm:bottom-[-5vw] md:bottom-[-15vw] lg:bottom-[-25vw]"
+        className="animate-spin-slow w-screen h-[100vw] md:w-[80vw] md:h-[80vw] bottom-0 sm:bottom-[-5vw] md:bottom-[-15vw] lg:bottom-[-25vw]"
         style={{
           position: "absolute",
           right: "-30vw",
